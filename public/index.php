89606e9eb0108f595fa12aeaa49bd08ac615b3f4
David Goodwin <david@palepurple.co.uk>
2017-07-20 14:03:03 +0100

    change signature check, add logging to logs/app.log if it exists and is writeable

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Monolog\Logger;
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -10,41 +11,58 @@ $app = new App();
 
 $container = $app->getContainer();
 
-$container['zendesk'] = function() {
+$logger = new Logger('zendesk-crm-integration');
+if (is_writeable(dirname(__FILE__) . '/logs/app.log')) {
+    $logger->pushHandler(new \Monolog\Handler\RotatingFileHandler(dirname(__FILE__) . '/logs/app.log', 10));
+} else {
+    $logger->pushHandler(new \Monolog\Handler\NullHandler());
+}
+
+$container['logger'] = $logger;
+
+$container['zendesk'] = function () {
     $client = new \Zendesk\API\HttpClient(getenv('ZENDESK_SUBDOMAIN'));
     $client->setAuth('basic', ['username' => getenv('ZENDESK_USERNAME'), 'token' => getenv('ZENDESK_TOKEN')]);
 
     return $client;
 };
 
-$container['twig'] = function() {
+$container['twig'] = function () {
     $loader = new Twig_Loader_Filesystem(__DIR__ . '/../template');
     return new Twig_Environment($loader, []);
 };
 
-$app->add(function(Request $request, Response $response, $next) {
+$app->add(function (Request $request, Response $response, $next) use ($app) {
+
     $sig = $request->getQueryParam('sig', null);
     $expires = $request->getQueryParam('expires', null);
 
+    /* @var Logger $logger */
+
+    $logger = $app->get('logger');
+
     if ($sig === null || $expires === null || $expires < time()) {
+        $logger->error("Request missing sig/expires, or expires < time", [$sig, $expires, time()]);
         return $response->withStatus(401);
     }
 
     $params = $request->getQueryParams();
     unset($params['sig']);
-    ksort($params);
 
-    if (hash_hmac('sha256', join(':', array_values($params)), getenv('SECRET')) !== $sig) {
+    $expected_sig = hash_hmac('sha256', http_build_query($params), getenv('SECRET'));
+
+    if ($expected_sig !== $sig) {
+        $logger->error("Request has invalid sig.", ['expected' => $expected_sig, 'actual' => $sig]);
         return $response->withStatus(401);
     }
 
     return $next($request, $response);
 });
 
-$app->get('/iframe', function(Request $request, Response $response) use ($app) {
+$app->get('/iframe', function (Request $request, Response $response) use ($app) {
 
     $id = $request->getQueryParam('id', null);
-    if (! isset($id) || empty($id)) {
+    if (!isset($id) || empty($id)) {
         throw new \InvalidArgumentException('Missing required param: id');
     }
 
@@ -64,10 +82,10 @@ $app->get('/iframe', function(Request $request, Response $response) use ($app) {
     return $response;
 });
 
-$app->get('/search', function(Request $request, Response $response) use ($app) {
+$app->get('/search', function (Request $request, Response $response) use ($app) {
 
     $query = $request->getQueryParam('q', null);
-    if (! isset($query) || empty($query)) {
+    if (!isset($query) || empty($query)) {
         throw new \InvalidArgumentException('Missing required param: q');
     }
 
@@ -77,7 +95,7 @@ $app->get('/search', function(Request $request, Response $response) use ($app) {
         'query' => $query
     ]);
 
-    $data = array_map(function($user) {
+    $data = array_map(function ($user) {
         return [
             'id' => $user->id,
             'name' => $user->name,
